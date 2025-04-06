223cf6026fd03b407f65b78048937d6e806d3e29
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-06 12:39:32 +0300

    Create 'Vacancy' model

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Vacancy(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    salary = models.PositiveIntegerField(null=True)
+    company = models.ForeignKey(
+        to="Company",
+        on_delete=models.CASCADE,
+        related_name="vacancies"
+    )
+    location = models.CharField(max_length=100, null=True)
+    contacts = models.CharField(max_length=100, null=True)
+    description = models.TextField()
