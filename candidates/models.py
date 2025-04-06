78f181f17b0edaf0b93abe7e2c49196b046d8f7b
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-06 13:26:34 +0300

    Create 'Candidate' user & Application model

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Vacancy(models.Model):
@@ -12,3 +13,30 @@ class Vacancy(models.Model):
     location = models.CharField(max_length=100, null=True)
     contacts = models.CharField(max_length=100, null=True)
     description = models.TextField()
+
+
+class Candidate(AbstractUser):
+    # profile_image = models.ImageField(upload_to="profile_images", null=True)
+    # resume = models.FileField(upload_to="resumes", null=True)
+    vacancies = models.ManyToManyField(to=Vacancy, related_name="candidates", through="Application")
+
+
+class Application(models.Model):
+    class Status(models.TextChoices):
+        APPLIED = "applied", "Applied"
+        INTERVIEWED = "interviewed", "Interviewed"
+        HIRED = "hired", "Hired"
+        REJECTED = "rejected", "Rejected"
+
+    candidate = models.ForeignKey(
+        to=Candidate,
+        on_delete=models.CASCADE,
+        related_name="applications"
+    )
+    vacancy = models.ForeignKey(
+        to=Vacancy,
+        on_delete=models.CASCADE,
+        related_name="applications"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default=Status.APPLIED)
