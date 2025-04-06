10bc69a38454837e604bbfc62c5e5d87e93cd11e
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-06 21:39:50 +0300

    Add vacancies list

--- candidates/urls.py
+++ candidates/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from candidates.views import index
-
+from candidates.views import index, JobsListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("jobs/", JobsListView.as_view(), name="jobs_list"),
 ]
 
 app_name = "candidates"
--- candidates/views.py
+++ candidates/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from candidates.models import Vacancy, Company
 
@@ -8,12 +10,20 @@ from candidates.models import Vacancy, Company
 def index(request: HttpRequest) -> HttpResponse:
     vacancies_count = Vacancy.objects.count()
     companies_count = Company.objects.count()
+    candidates_count = get_user_model().objects.count()
     context = {
         "vacancies_count": vacancies_count,
         "companies_count": companies_count,
+        "candidates_count": candidates_count,
     }
     return render(
         request,
         "candidates/index.html",
         context=context,
     )
+
+
+class JobsListView(ListView):
+    model = Vacancy
+    context_object_name = "vacancies"
+    template_name = "candidates/vacancies_list.html"
