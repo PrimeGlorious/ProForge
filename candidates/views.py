2f5853454da38a5d0a1cb30639313a6afa9da335
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-06 20:49:59 +0300

    Create home page

--- candidates/views.py
+++ candidates/views.py
@@ -2,6 +2,18 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from candidates.models import Vacancy, Company
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. This is the index page.")
+    vacancies_count = Vacancy.objects.count()
+    companies_count = Company.objects.count()
+    context = {
+        "vacancies_count": vacancies_count,
+        "companies_count": companies_count,
+    }
+    return render(
+        request,
+        "candidates/index.html",
+        context=context,
+    )
