4e8736ea232d6eb4b8c1718fb815342ca156a65f
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-18 16:32:40 +0300

    rename SECRET_KEY environ

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Application definition
 
